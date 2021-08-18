91c4d9fecee426d9416c7976ef4369bceb3a495e
Abdullah Furkan Titiz <65949007+afurkantitiz@users.noreply.github.com>
2021-08-18 15:58:49 +0300

    Restaurant backend finished

---
+++ app/src/main/java/com/afurkantitiz/foodapp/data/entity/profile/UserResponse.kt
@@ -0,0 +1,10 @@
+package com.afurkantitiz.foodapp.data.entity.profile
+
+import com.google.gson.annotations.SerializedName
+
+data class UserResponse(
+    @SerializedName("data")
+    val user: User,
+    @SerializedName("success")
+    val success: Boolean
+)
